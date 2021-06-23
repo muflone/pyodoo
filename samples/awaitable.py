c86a9ba1d88d9c84ebf0685b1ea0e2a32d9f7f5b
Muflone <webreg@vbsimple.net>
2021-06-24 01:14:34 +0200

    Added some samples for async usages using awaitable

---
+++ samples/awaitable.py
@@ -0,0 +1,37 @@
+##
+#     Project: PyOdoo
+# Description: API for Odoo
+#      Author: Fabio Castelli (Muflone) <muflone@muflone.com>
+#   Copyright: 2021 Fabio Castelli
+#     License: GPL-3+
+#  This program is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+##
+
+import asyncio
+import functools
+
+
+def awaitable(func):
+    """
+    Decorates a synchronous function to run it in asynchronous
+    :param func: function to decorate
+    :return: the awaited data from the decorated function
+    """
+    @functools.wraps(func)
+    async def run(*args, loop=None, executor=None, **kwargs):
+        if loop is None:
+            loop = asyncio.get_event_loop()
+        pfunc = functools.partial(func, *args, **kwargs)
+        return await loop.run_in_executor(executor, pfunc)
+    return run
---
+++ samples/contacts_async.py
@@ -0,0 +1,121 @@
+##
+#     Project: PyOdoo
+# Description: API for Odoo
+#      Author: Fabio Castelli (Muflone) <muflone@muflone.com>
+#   Copyright: 2021 Fabio Castelli
+#     License: GPL-3+
+#  This program is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+##
+
+import asyncio
+import datetime
+import os
+import time
+
+from pyodoo.v12 import Model
+
+from awaitable import awaitable
+
+
+def measure_time(name: str, start_time: int) -> None:
+    """
+    Print elapsed time since start time
+
+    :param name: task name
+    :param start_time: start time
+    """
+    duration = int((time.time() - start_time) * 1000)
+    print(f'Duration for {name}: {duration} ms')
+
+
+def update_many(minimum: int, maximum: int) -> None:
+    for i in range(minimum, maximum):
+        update_one(i)
+
+
+def update_one(entity_id: int) -> None:
+    now = datetime.datetime.now().strftime('%d/%M/%Y %H:%m.%S')
+    model.update(entity_id=entity_id,
+                 values={'name': f'test {now}'})
+
+
+async def update_many_async(minimum: int, maximum: int) -> None:
+    tasks = []
+    for i in range(minimum, maximum):
+        tasks.append(update_one_async(i))
+    await asyncio.gather(*tasks)
+
+
+@awaitable
+def update_one_async(entity_id: int) -> None:
+    now = datetime.datetime.now().strftime('%d/%M/%Y %H:%m.%S')
+    model.update(entity_id=entity_id,
+                 values={'name': f'test {now}'})
+
+
+def find_many(entity_ids: list[int]) -> None:
+    results = []
+    for entity_id in entity_ids:
+        results.append(find_one([entity_id]))
+    return results
+
+
+def find_one(entity_ids: list[int]) -> list[dict[str, str]]:
+    results = model.find(entity_ids=entity_ids,
+                         fields=('id', 'name', 'country_id'))
+    return results
+
+
+async def find_many_async(entity_ids: list[int]) -> None:
+    tasks = []
+    for entity_id in entity_ids:
+        tasks.append(find_one_async([entity_id]))
+    results = await asyncio.gather(*tasks)
+    return results
+
+
+@awaitable
+def find_one_async(entity_ids: list[int]) -> list[dict[str, str]]:
+    results = model.find(entity_ids=entity_ids,
+                         fields=('id', 'name', 'country_id'))
+    return results
+
+
+# Instance model object
+model = Model(model_name='res.partner',
+              endpoint=os.environ['ODOO_ENDPOINT'],
+              database=os.environ['ODOO_DATABASE'],
+              username=os.environ['ODOO_USERNAME'],
+              password=os.environ['ODOO_PASSWORD'],
+              language=None)
+# Authenticate user
+model.authenticate()
+# Change default language
+model.language = 'en_GB'
+
+start_time = time.time()
+results = find_many(list(range(24540, 24550)))
+measure_time(name='find_many', start_time=start_time)
+
+start_time = time.time()
+results = asyncio.run(find_many_async(list(range(24540, 24550))))
+measure_time(name='find_many_async', start_time=start_time)
+
+start_time = time.time()
+update_many(minimum=24540, maximum=24550)
+measure_time(name='update_many', start_time=start_time)
+
+start_time = time.time()
+asyncio.run(update_many_async(minimum=24540, maximum=24550))
+measure_time(name='update_many_async', start_time=start_time)
