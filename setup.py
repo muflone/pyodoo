0c12fb1545285132e173a39be00f88f4c9e500f2
Muflone <webreg@muflone.com>
2022-06-12 03:41:39 +0200

    Added Python 3.10 to supported versions

--- setup.py
+++ setup.py
@@ -47,6 +47,7 @@ setuptools.setup(
         'Operating System :: OS Independent',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
         'Topic :: Software Development :: Libraries :: Python Modules'
     ],
 )
