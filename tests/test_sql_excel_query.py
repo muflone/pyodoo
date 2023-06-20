dc939e4eee11b86e55fbb9754b9a07414eaefbd2
Muflone <webreg@muflone.com>
2023-06-21 01:40:24 +0200

    Added some initial tests for SqlExcelQuery

---
+++ tests/test_sql_excel_query.py
@@ -0,0 +1,90 @@
+##
+#     Project: PyOdoo
+# Description: API for Odoo
+#      Author: Fabio Castelli (Muflone) <muflone@muflone.com>
+#   Copyright: 2021-2023 Fabio Castelli
+#     License: GPL-3+
+#  This program is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+##
+
+import unittest
+import uuid
+
+from pyodoo.constants import APP_NAME, APP_VERSION
+from pyodoo.v12 import SqlExcelQuery
+
+import utility
+
+
+class TestCaseSqlExcelQuery(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        """
+        Query object preparation
+        """
+        info = utility.get_authentication_from_demo()
+        if SqlExcelQuery.is_available(endpoint=info['host'],
+                                      database=info['database'],
+                                      username=info['user'],
+                                      password=info['password'],
+                                      language='en_US'):
+            # The model sql.excel.pdf is available
+            cls.query_name = uuid.uuid4().hex
+            cls.query = SqlExcelQuery(name=cls.query_name,
+                                      category=f'{APP_NAME} {APP_VERSION}',
+                                      endpoint=info['host'],
+                                      database=info['database'],
+                                      username=info['user'],
+                                      password=info['password'],
+                                      language='en_US')
+        else:
+            # The model sql.excel.pdf is not available
+            cls.query = None
+
+    def setUp(self):
+        """
+        Check if the test could be run
+        """
+        if not self.query:
+            self.skipTest('Model sql.excel.pdf is not available')
+
+    def test_01_get_name(self) -> None:
+        """
+        Check the query name
+        """
+        results = self.query.name
+        # Check if the name is not None
+        self.assertIsNotNone(results)
+        # Check if the name is the same
+        self.assertEqual(results, self.query_name)
+
+    def test_02_get_language(self) -> None:
+        """
+        Check the query language
+        """
+        results = self.query.language
+        # Check if the language is not None
+        self.assertIsNotNone(results)
+        # Check if the language is the same
+        self.assertEqual(results, 'en_US')
+
+    def test_03_get_id(self) -> None:
+        """
+        Check the query ID
+        """
+        results = self.query.id
+        # Check if the ID is not None
+        self.assertIsNotNone(results)
+        # Check if the ID is > 0
+        self.assertGreater(results, 0)
